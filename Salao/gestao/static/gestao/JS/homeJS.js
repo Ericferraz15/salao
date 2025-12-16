6d6287b2c67b5e6a2fa1e31788ac8fd9d3e1c9fe
Eric Ferraz <ericferraz4@gmail.com>
2025-12-16 08:45:20 -0300

    add imgs statics e cria o movimento das imagens

--- Salao/gestao/static/gestao/JS/homeJS.js
+++ Salao/gestao/static/gestao/JS/homeJS.js
@@ -1,4 +1,3 @@
-        // Menu Mobile
         const mobileMenuBtn = document.getElementById('mobileMenuBtn');
         const navLinks = document.getElementById('navLinks');
         
@@ -14,7 +13,6 @@
             }
         });
         
-        // Fechar menu ao clicar em um link
         document.querySelectorAll('.nav-links a').forEach(link => {
             link.addEventListener('click', () => {
                 navLinks.classList.remove('active');
@@ -24,7 +22,6 @@
             });
         });
         
-        // Efeito de scroll suave para links internos
         document.querySelectorAll('a[href^="#"]').forEach(anchor => {
             anchor.addEventListener('click', function(e) {
                 e.preventDefault();
@@ -41,3 +38,34 @@
                 }
             });
         });
+
+        const imagens1 = [
+            'static/images/fibraDeVidro.jpg',
+            'static/images/fibraDeVidro2.jpg',
+            'static/images/fibraDeVidro3.jpg',
+        ];  
+        const imagens2 = [  
+            'static/images/esmaltacao.jpg',
+            'static/images/esmaltacao2.jpg',
+            'static/images/esmaltacao3.jpg',
+        ];
+        const imagens3 = [
+            'static/images/banho.jpg',
+            'static/images/banho2.jpg',
+            'static/images/banho3.jpg',
+        ]
+function iniciarSlide(idElemento, listaImagens, tempo) {
+    let indice = 0;
+    const elemento = document.getElementById(idElemento);
+    
+    if(!elemento) return; 
+
+    setInterval(() => {
+        indice = (indice + 1) % listaImagens.length;
+        elemento.src = listaImagens[indice];
+    }, tempo);
+}
+
+iniciarSlide('card1', imagens1, 3000);
+iniciarSlide('card2', imagens2, 3000);
+iniciarSlide('card3', imagens3, 3000);
